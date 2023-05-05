55336d5eacd50b8265ff44f85e64f421c29486d7
effoeffi <effoeffi@gmail.com>
2023-05-05 19:44:51 +0300

    env

---
+++ env/env.go
@@ -0,0 +1,72 @@
+package env
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+const Project = "democracy-tools"
+
+func GetSmtp() string {
+
+	const key = "SMTP"
+	res := os.Getenv(key)
+	if res == "" {
+		res = "smtp.gmail.com"
+	}
+	logrus.Debugf("%s: %s", key, res)
+
+	return res
+}
+
+func GetEmailSupport() string {
+
+	return failIfEmpty("EMAIL_SUPPORT")
+}
+
+func GetEmailFrom() string {
+
+	return failIfEmpty("EMAIL_FROM")
+}
+
+func GetEmailPassword() string {
+
+	return failIfEmpty("EMAIL_PASSWORD")
+}
+
+func GetWhatsAppVerificationToken() string {
+
+	return failIfEmpty("WHATSAPP_VERIFICATION_TOKEN")
+}
+
+func GetWhatAppToken() string {
+
+	return failIfEmpty("WHATSAPP_TOKEN")
+}
+
+func GetWhatsAppFromPhone() string {
+
+	return failIfEmpty("WHATSAPP_FROM_PHONE")
+}
+
+func GetSlackInfoUrl() string {
+
+	return failIfEmpty("SLACK_INFO_URL")
+}
+
+func GetSlackDebugUrl() string {
+
+	return failIfEmpty("SLACK_DEBUG_URL")
+}
+
+func failIfEmpty(key string) string {
+
+	res := os.Getenv(key)
+	if res == "" {
+		logrus.Fatalf("Please, add environment variable '%s'", key)
+	}
+	logrus.Debugf("%s: %s", key, res)
+
+	return res
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/democracy-tools/go-common
+
+go 1.20
+
+require github.com/sirupsen/logrus v1.9.0
+
+require (
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
